0665c8d4063a48104f3907db6c97a98adb2b4a5f
ooperlooper <navonod.neirbo@gmail.com>
2023-05-17 23:47:32 +0000

    Third time's the charm.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,7 +27,7 @@ DJANGO_SUPERUSER_PASSWORD = os.environ['PASSWORD']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1']
 
 
 # Application definition
