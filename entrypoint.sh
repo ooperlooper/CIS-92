a3684cbe0ddf80156220ce56ec0e7a7d1b0d3daf
ooperlooper <navonod.neirbo@gmail.com>
2023-05-17 23:29:08 +0000

    fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 python ./manage.py migrate --noinput
 python ./manage.py initadmin student student@cis.cabrillo.edu Cabri11o
-python ./manage.py runserver 172.0.0.1:8000
+python ./manage.py runserver 0:8000
 
