cf773fe4e594196adfd0a721d1fa3a5e9d9cc475
ooperlooper <navonod.neirbo@gmail.com>
2023-05-20 22:11:50 +0000

    test

---
+++ dbuild.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+# This is the testing script!
+docker build -t mysite .
+(cd mysite/;docker run -it --rm -e SECRET_KEY='xxxyyy' -e PORT=8080 -p 8080:8000 mysite)
---
+++ gpush.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+# This is the testing script!
+
+git add --all 
+git commit -m "test" 
+git push
