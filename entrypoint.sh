3fd11b1cd4a0ab2a5a209056e4ab2a3047d52836
ooperlooper <navonod.neirbo@gmail.com>
2023-05-21 02:21:56 +0000

    nosecretsused

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 
 python ./manage.py migrate --noinput
-python ./manage.py initadmin $ANAME $AEMAIL $APASS
-python ./manage.py runserver 0.0.0.0:$PORT
+python ./manage.py initadmin student student@cis.cabrillo.edu Cabri11o
+python ./manage.py runserver 0.0.0.0:8000
 
 
 
